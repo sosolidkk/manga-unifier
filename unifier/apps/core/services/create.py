7efc1af5cde8f0ca211f56dbcc1316ba7dbb90a4
sosolidkk <pedrochaveslimas3@gmail.com>
2021-02-15 23:42:58 -0300

    Add base command abc and create models services

---
+++ unifier/apps/core/services/create.py
@@ -0,0 +1,66 @@
+import logging
+
+from unifier.apps.core.models import Image, Manga, MangaChapter, Novel, NovelChapter, Platform
+from unifier.apps.core.services.base import Command
+
+logger = logging.getLogger(__name__)
+
+
+class CreateImageService(Command):
+    def __init__(self, payload: dict) -> None:
+        self._payload = payload
+
+    def execute(self) -> Image:
+        logger.info(f"Creating image: {self._payload}")
+        image = Image.objects.create(**self._payload)
+        return image
+
+
+class CreateMangaService(Command):
+    def __init__(self, payload: dict) -> None:
+        self._payload = payload
+
+    def execute(self) -> Manga:
+        logger.info(f"Creating manga: {self._payload}")
+        manga = Manga.objects.create(**self._payload)
+        return manga
+
+
+class CreateMangaChapterService(Command):
+    def __init__(self, payload: dict) -> None:
+        self._payload = payload
+
+    def execute(self) -> MangaChapter:
+        logger.info(f"Creating manga chapter: {self._payload}")
+        manga_chapter = MangaChapter.objects.create(**self._payload)
+        return manga_chapter
+
+
+class CreateNovelService(Command):
+    def __init__(self, payload: dict) -> None:
+        self._payload = payload
+
+    def execute(self) -> Novel:
+        logger.info(f"Creating novel: {self._payload}")
+        novel = Novel.objects.create(**self._payload)
+        return novel
+
+
+class CreateNovelChapterService(Command):
+    def __init__(self, payload: dict) -> None:
+        self._payload = payload
+
+    def execute(self) -> NovelChapter:
+        logger.info(f"Creating novel chapter: {self._payload}")
+        novel_chapter = NovelChapter.objects.create(**self._payload)
+        return novel_chapter
+
+
+class CreatePlatformService(Command):
+    def __init__(self, payload: dict) -> None:
+        self._payload = payload
+
+    def execute(self) -> Platform:
+        logger.info(f"Creating platform: {self._payload}")
+        platform = Platform.objects.create(**self._payload)
+        return platform
