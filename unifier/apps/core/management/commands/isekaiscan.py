b5307e2e7f5bba84537c22da8767cd09cd266b98
sosolidkk <pedrochaveslimas3@gmail.com>
2021-03-02 17:27:36 -0300

    Refactor on isekaiscan command and add http adapters support files for requests

--- unifier/apps/core/management/commands/isekaiscan.py
+++ unifier/apps/core/management/commands/isekaiscan.py
@@ -1,15 +1,16 @@
 import logging
 import re
 
-import requests
 from bs4 import BeautifulSoup
-from bs4.element import ResultSet, Tag
+from bs4.element import Tag
 from django.core.management.base import BaseCommand
 from django.utils.text import slugify
-from requests.models import Response
 from unifier.apps.core.models import Manga, MangaChapter, Platform
 from unifier.apps.core.models.chapter import Language
 from unifier.apps.core.services import BulkCreateMangaChapterService
+from unifier.support.http import http
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -23,62 +24,78 @@ class Command(BaseCommand):
             for manga in mangas:
                 manga_info = {}
                 chapters_content = []
-
-                data = {"action": "manga_get_chapters", "manga": None}
-                headers = {
-                    "User-Agent": (
-                        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
-                        "(KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36"
-                    )
-                }
+                payload = {"action": "manga_get_chapters", "manga": None}
 
                 self.stdout.write(f"Current manga: {manga}")
 
-                response = requests.get(f"{platform.url}manga/{slugify(manga.title)}")
-                response.raise_for_status()
+                response = http.get(f"{platform.url}manga/{slugify(manga.title)}")
                 content = BeautifulSoup(response.text, "html.parser")
-                shortlink = content.find("link", {"rel": "shortlink"}).attrs["href"]
-                data["manga"] = int(shortlink.split("=")[-1])
 
-                post_content = content.find("div", {"class": "post-content"})
-                post_status = content.find("div", {"class": "post-status"})
+                payload["manga"] = self._find_manga_id(content)
+                manga_info = {**self._find_manga_info(content)}
 
-                post_content_items = post_content.find_all("div", {"class": "post-content_item"})
-                for item in post_content_items:
-                    if "author" in item.text.lower():
-                        manga_info["author"] = item.find("div", {"class": "author-content"}).text.strip()
-                    if "genre" in item.text.lower():
-                        manga_info["tags"] = item.find("div", {"class": "genres-content"}).text.strip().split(",")
-
-                manga_info["status"] = post_status.find("div", {"class": "summary-content"}).text.strip()
-
-                response = requests.post(platform.url_search, data=data, headers=headers)
-                response.raise_for_status()
+                response = http.post(platform.url_search, data=payload)
                 content = BeautifulSoup(response.text, "html.parser")
+
                 chapters_urls = content.find_all("a", attrs={"href": True, "class": False})
-
                 manga_info["chapters_count"] = len(chapters_urls)
 
-                if manga.chapters_count == manga_info["chapters_count"]:
+                if not self._has_new_chapters(manga, manga_info["chapters_count"]):
                     self.stdout.write(f"{manga} don't have any new chapters")
                     continue
 
                 for url in chapters_urls:
-                    data = {}
-                    data["title"] = url.text.strip()
-                    data["number"] = int(re.findall(r"\d+", url.text.strip())[0])
-                    data["language"] = Language.ENGLISH_US
-                    data["manga"] = manga
-
+                    data = self._find_chapter_info(url, manga)
                     self.stdout.write(f"Chapter: {data}")
 
-                    _url = url.attrs["href"]
-                    response = requests.get(_url, headers=headers)
+                    response = http.get(data["url"])
                     content = BeautifulSoup(response.text, "html.parser")
-                    images_div = content.find("div", {"class": "reading-content"})
-                    data["images"] = [image.attrs["data-src"].strip() for image in images_div.find_all("img")]
+
+                    data["images"] = self._find_chapter_images(content)
+                    del data["url"]
+
                     chapters_content.append(data)
 
                 BulkCreateMangaChapterService([MangaChapter(**chapter) for chapter in chapters_content]).execute()
                 manga.__dict__.update(**manga_info)
                 manga.save()
+
+    def _has_new_chapters(self, manga: Manga, chapters_count: int):
+        if manga.chapters_count == chapters_count:
+            return False
+        return True
+
+    def _find_manga_info(self, content: BeautifulSoup) -> dict:
+        _manga_info = {}
+
+        post_content = content.find("div", {"class": "post-content"})
+        post_status = content.find("div", {"class": "post-status"})
+        post_content_items = post_content.find_all("div", {"class": "post-content_item"})
+
+        for item in post_content_items:
+            if "author" in item.text.lower():
+                _manga_info["author"] = item.find("div", {"class": "author-content"}).text.strip()
+            if "genre" in item.text.lower():
+                _manga_info["tags"] = item.find("div", {"class": "genres-content"}).text.strip().split(",")
+        _manga_info["status"] = post_status.find("div", {"class": "summary-content"}).text.strip()
+
+        return _manga_info
+
+    def _find_manga_id(self, content: BeautifulSoup) -> int:
+        shortlink = content.find("link", {"rel": "shortlink"}).attrs["href"]
+        return int(shortlink.split("=")[-1])
+
+    def _find_chapter_info(self, element: Tag, manga: Manga) -> dict:
+        data = {}
+        data["title"] = element.text.strip()
+        data["number"] = int(re.findall(r"\d+", element.text.strip())[0])
+        data["language"] = Language.ENGLISH_US
+        data["manga"] = manga
+        data["url"] = element.attrs["href"]
+        data["images"] = []
+
+        return data
+
+    def _find_chapter_images(self, content: BeautifulSoup) -> list:
+        images_div = content.find("div", {"class": "reading-content"})
+        return [image.attrs["data-src"].strip() for image in images_div.find_all("img")]
