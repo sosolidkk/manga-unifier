b418ea9183c1aecc61b3ae414555806db311d9d7
sosolidkk <pedrochaveslimas3@gmail.com>
2021-03-23 00:25:57 -0300

    Add delete user route

--- unifier/apps/drf/v1/views/user.py
+++ unifier/apps/drf/v1/views/user.py
@@ -1,5 +1,6 @@
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 from unifier.apps.core.models import User
 from unifier.apps.drf.v1.serializers import UserSerializer
 
@@ -8,3 +9,13 @@ class UserCreateAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
+
+
+class UserDeleteAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def destroy(self, request, *args, **kwargs):
+        self.request.user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
