1696d13a6986348ec58b2b84f408949ab7ef9ff2
sosolidkk <pedrochaveslimas3@gmail.com>
2021-05-05 15:17:42 -0300

    Add comment

--- unifier/apps/core/apps.py
+++ unifier/apps/core/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = "unifier.apps.core"
-
+    # Just a comment
     def ready(self):
         from unifier.apps.core.signals import create_auth_token
