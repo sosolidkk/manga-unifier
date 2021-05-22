e49b3c9b2dca73f8674fb21362d1d51643b19590
Max Lima <max.lima2@gmail.com>
2021-05-22 18:25:12 -0300

    change page size og pagination to 25

--- unifier/apps/drf/v1/pagination.py
+++ unifier/apps/drf/v1/pagination.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class BasePagination(PageNumberPagination):
-    page_size = 50
+    page_size = 25
     page_size_query_param = "page_size"
-    max_page_size = 50
+    max_page_size = 25
